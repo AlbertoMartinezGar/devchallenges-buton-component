796d0fe0479aa5e443f06ba6be61777bd198f00a
AlbertoMartinezGar <albert.mtzz2003@gmail.com>
2022-05-24 13:16:16 -0500

    More icons

--- src/components/IconsSection.js
+++ src/components/IconsSection.js
@@ -6,25 +6,115 @@ const IconsSection = () => {
     <div className='row mt-5'>
         <h1>Icons</h1>
         <hr />
-        <div className='col-6 d-flex justify-content-center'>
-          <ButtonComponent 
-            text={'Icon'} 
-            type={'default'}
-            color={'primary'}
-            size={'lg'}
-            icon={'pending'}
-            iconDirection={'right'}
-          />
+        <div className='row mb-3'>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'pending'}
+              iconDirection={'right'}
+            />
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'pending'}
+              iconDirection={'left'}
+            />
+          </div>
         </div>
-        <div className='col-6 d-flex justify-content-center'>
-          <ButtonComponent 
-            text={'Icon'} 
-            type={'default'}
-            color={'primary'}
-            size={'lg'}
-            icon={'pending'}
-            iconDirection={'left'}
-          />
+        <div className='row mb-3'>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'favorite'}
+              iconDirection={'right'}
+            />
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'favorite'}
+              iconDirection={'left'}
+            />
+          </div>
+        </div>
+        <div className='row mb-3'>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'verified'}
+              iconDirection={'right'}
+            />
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'primary'}
+              size={'lg'}
+              icon={'verified'}
+              iconDirection={'left'}
+            />
+          </div>
+        </div>
+        <div className='row mb-3'>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'secondary'}
+              size={'lg'}
+              icon={'build'}
+              iconDirection={'right'}
+            />
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'secondary'}
+              size={'lg'}
+              icon={'build'}
+              iconDirection={'left'}
+            />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'danger'}
+              size={'lg'}
+              icon={'highlight_off'}
+              iconDirection={'right'}
+            />
+          </div>
+          <div className='col-6 d-flex justify-content-center'>
+            <ButtonComponent 
+              text={'Icon'} 
+              type={'default'}
+              color={'danger'}
+              size={'lg'}
+              icon={'highlight_off'}
+              iconDirection={'left'}
+            />
+          </div>
         </div>
       </div>
   )
